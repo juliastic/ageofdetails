7e2ad2d4bfa9064428439e60632fb7763b0ccc79
Julia Grill <jusogrill@gmail.com>
2020-11-15 08:15:25 +0100

    Initial Commit

---
+++ AgeOfDetails/AgeOfDetailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgeOfDetailsApp.swift
+//  AgeOfDetails
+//
+//  Created by Julia Grill on 15.11.20.
+//
+
+import SwiftUI
+
+@main
+struct AgeOfDetailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AgeOfDetails/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AgeOfDetails
+//
+//  Created by Julia Grill on 15.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
